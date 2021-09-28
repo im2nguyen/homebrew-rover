46f6204d758119162fb3f0625d4954b78665ad01
Tu Nguyen <im2nguyen@gmail.com>
2021-09-28 04:46:40 -0700

    Version 0.2.0

---
+++ rover.rb
@@ -0,0 +1,18 @@
+class Envo < Formula
+  desc "Interactive Terraform visualization. State and configuration explorer."
+  homepage "https://github.com/im2nguyen/rover"
+  url "https://github.com/im2nguyen/rover/releases/download/v0.2.0/rover-0.2.0.tar.gz"
+  sha256 "0f301097d42d6e391d20024a142c90e3cebd67ea6dc0070918bf835688b8bfc6"
+  license "MIT"
+
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "envo"
+  end
+
+  test do
+    system "false"
+  end
+end
